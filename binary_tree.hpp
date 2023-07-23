104240f3b3b9c5a6d50446e4934b73a820084a41
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-23 14:25:19 -0300

    binary tree header

---
+++ binary_tree.hpp
@@ -0,0 +1,36 @@
+#ifndef BINARY_TREE_HPP
+#define BINARY_TREE_HPP
+#include <iostream>
+#include "node.hpp"
+
+const int capacity = 5;
+
+class BinaryTree
+{
+public:
+    BinaryTree();
+    ~BinaryTree();
+
+    void insert(int value);
+
+    void inOrderTraversal();
+
+    void preOrderTraversal();
+
+    void postOrderTraversal();
+
+    int maxDepth();
+
+private:
+    Node *insertRecursive(Node *current, int value);
+
+    void inOrderRecursive(Node *current);
+
+    void preOrderRecursive(Node *current);
+
+    void postOrderRecursive(Node *current);
+
+    int maxDepthRecursive(Node *current);
+};
+
+#endif
